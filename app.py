dd69b583edac66808d521247dfcf855b7980b742
pablotose <pablotoledosegundo@gmail.com>
2021-05-26 18:54:36 +0200

    Aplicación de adopción de animales usando bottle py y cassandra

---
+++ app.py
@@ -0,0 +1,41 @@
+from bottle import run, route, get, post, hook, request, redirect, template
+
+from cassandra.cluster import Cluster
+
+cluster = Cluster(['84.122.138.226'])
+
+@hook('before_request')
+def strip_trailing_slash():
+	request.environ['PATH_INFO'] = request.environ['PATH_INFO'].rstrip('/')
+
+
+@route('/')
+def index():
+	session = cluster.connect()
+	session.set_keyspace("db")
+	q = request.GET.get('q')
+	if q:
+		query = session.execute("SELECT * from animales where nombre_animal like '{}%'".format(q))
+		return template('templates/layout.html', animals = query) 
+	else:
+		data = session.execute("SELECT * from animales")
+		return template('templates/layout.html', animals = data)
+
+@post('/add_animal', method='POST')
+def add_animal():
+	if request.method == 'POST':
+		nombre_animal = str(request.params.get('name_animal'))
+		print (nombre_animal)
+		localidad = str(request.params.get('localidad'))
+		edad_animal = int(request.params.get('edad'))
+		tipo_animal = str(request.params.get('tipo'))
+		session = cluster.connect()
+		session.set_keyspace("db")
+		session.execute("INSERT INTO animales (id_animal, nombre_animal, localidad, edad_animal, tipo_animal) values (now(), '{}','{}',{},'{}')".format(nombre_animal, localidad, edad_animal, tipo_animal))
+	
+		return redirect('/')
+
+
+
+
+run(host='localhost', port=8080, debug=True, reloader=True)
